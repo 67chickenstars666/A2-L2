f0c9e8f280ab0903e28999de77d2dc71e112b6e2
67chickenstars666 <faatehamad@gmail.com>
2025-11-24 17:50:47 +0500

    typecasting main

---
+++ main.py
@@ -0,0 +1,13 @@
+name="pengu"
+age="10"
+is_stdent=True
+weight=29.7
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", is_stdent)
+print("Weight:", weight)
+print("/n After Type Casting:")
+age=str(age)
+weight=int(weight)
+print(weight)
+print("Data Type of weight:", type(weight))
