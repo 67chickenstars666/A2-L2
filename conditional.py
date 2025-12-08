a4e93affa4ce7bc1730428fda1fe57629ad0bfc1
67chickenstars666 <faatehamad@gmail.com>
2025-12-08 17:51:38 +0500

    abcdefghrkrjkjkgrjgklrjkrgjgktjkgk

--- conditional.py
+++ conditional.py
@@ -11,4 +11,17 @@ if num < 0:
         print("Total profit= {0}".format(amount))
     else:
         print("No Profit :(")
-        
+# checking the number greater or smaller than fifteen
+i=int(input("enter a number: "))
+if (i<15):
+    print("number is smaller than 15")
+else:
+    print("number is greater than 15")
+    print("I'm in else block")
+print("I'm out of else block ")
+number = int(input("Enter a number to check: "))
+print("Number to be checked :", number)
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
