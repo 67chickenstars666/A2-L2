04eb6bf530884e65aaf45597d5e4508e065e2008
67chickenstars666 <faatehamad@gmail.com>
2025-12-08 17:27:19 +0500

    Co-authored-by: Rida Zulfiqar <unculturedrida@users.noreply.github.com>

---
+++ conditional.py
@@ -0,0 +1,14 @@
+num=3
+if num > 0:
+    print("Positive number")
+num=-1
+if num < 0:
+    print("Negative number")
+    actual_cost=float(input("Please enter the actual cost: "))
+    sale_amount=float(input("Please enter the sale amount: "))
+    if sale_amount > actual_cost:
+        amount=sale_amount-actual_cost
+        print("Total profit= {0}".format(amount))
+    else:
+        print("No Profit :(")
+        
