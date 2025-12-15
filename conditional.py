9ca4a01c00ff91178c300f421e7f1434fa0bc89e
67chickenstars666 <faatehamad@gmail.com>
2025-12-15 20:49:55 +0500

    6767

--- conditional.py
+++ conditional.py
@@ -1,13 +1,5 @@
-num=50
-if num > 0:
-    print("Positive number")
-num=-1
-if num < 0:
-    print("Negative number")
-    actual_cost=float(input("Please enter the actual cost: "))
-    sale_amount=float(input("Please enter the sale amount: "))
-    if sale_amount > actual_cost:
-        amount=sale_amount-actual_cost
-        print("Total profit= {0}".format(amount))
-    else:
-        print("No Profit :(")
+temperature = 12  
+if temperature >= 15:
+    print("you do not need to wear jacket")
+else:
+    print("you have to wear jacket")
