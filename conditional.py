d046e510ea6240964cbcac89f9649358d0b18c57
67chickenstars666 <faatehamad@gmail.com>
2025-12-21 12:37:16 +0500

    operators

--- conditional.py
+++ conditional.py
@@ -1,5 +1,14 @@
-temperature = 12  
-if temperature >= 15:
-    print("you do not need to wear jacket")
+a =10
+b =18
+c = 6
+if a and b and c:
+    print("all numbers are boolean value as true")
 else:
-    print("you have to wear jacket")
+    print("at least one number is boolean value as false") 
+a = 666
+b = 89
+c = 44
+if a>0 or b>0 or c>0:
+          print("either of the numbers are greater than zero")
+else:
+           print("none of the numbers are greater than zero")
