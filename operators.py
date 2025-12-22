2d2a9c1a74451c3051dbe0791ca01804b8217e53
67chickenstars666 <faatehamad@gmail.com>
2025-12-22 17:35:52 +0500

    operators.py III

---
+++ operators.py
@@ -0,0 +1,16 @@
+x=3
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+y=7.4
+if type(y) is not int:
+    print("true")
+else:
+    print("false")
+x=777
+y=41141
+if x is  y:
+    print("true")
+if x is not y:
+    print("false")
